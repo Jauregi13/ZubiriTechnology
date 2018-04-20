264ea54e22d1051bf96d01dbdef0930b16c1f4f5
jauregi13 <jon.j1305@gmail.com>
2018-04-20 13:50:16 +0200

    primer commit

---
+++ src/modelo/Cliente.java
@@ -0,0 +1,51 @@
+package modelo;
+
+import java.util.Date;
+
+public class Cliente {
+	
+	public int id;
+	public String nombre;
+	public String apellidos;
+	public String dni;
+	public Date fechaNacimiento;
+	
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	
+	
+	
+
+}
---
+++ src/modelo/Config.java
@@ -0,0 +1,10 @@
+package modelo;
+
+public interface Config {
+	
+	public static final String HOST= "localhost";
+	public static final String BBDD= "zubiritechnology";
+	public static final String USER= "root";
+	public static final String PASSWORD= "";
+
+}
