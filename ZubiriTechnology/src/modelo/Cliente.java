c1d8f50bbae82443e97dc3c4d302911cec8fd5b3
ik012982i9 <ik012982i9@20604-HZ336135.H012982.NET>
2018-04-23 08:43:41 +0200

    Commit ProductoModelo

---
+++ ZubiriTechnology/src/modelo/Cliente.java
@@ -0,0 +1,51 @@
+package modelo;
+
+import java.util.Date;
+
+public class Cliente {
+	
+	private int id;
+	private String nombre;
+	private String apellidos;
+	private String dni;
+	private Date fechaNacimiento;
+	
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	
+	
+	
+
+}
---
+++ ZubiriTechnology/src/modelo/Producto.java
@@ -0,0 +1,63 @@
+package modelo;
+
+public class Producto {
+	
+	private int id;
+	private int id_categoria;
+	private String nombre;
+	private int stock;
+	private String tipo;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// LISTADO DE GETTERS & SETTERS
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getId_categoria() {
+		return id_categoria;
+	}
+	public void setId_categoria(int id_categoria) {
+		this.id_categoria = id_categoria;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+	// Metodo para añadir producto
+	public void add(Producto producto) {
+		
+		
+	}
+	
+	
+	
+	
+
+}
